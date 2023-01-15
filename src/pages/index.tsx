affb912faa909190ae748249e90dd10469caa85c
Monroe Clinton <contact@monroeclinton.com>
2023-01-15 10:07:50 -0500

    JSX fix: stroke-width should be strokeWidth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,7 +31,7 @@ const Home: NextPage = () => {
                             cy="12"
                             r="10"
                             stroke="currentColor"
-                            stroke-width="4"
+                            strokeWidth="4"
                         ></circle>
                         <path
                             fill="currentColor"
