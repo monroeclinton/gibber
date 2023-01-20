f6ebd18bb0265e26ce353be301bceb9a06072fbc
Monroe Clinton <contact@monroeclinton.com>
2023-01-19 22:41:52 -0500

    Create Button component

---
+++ src/components/buttons/Button.tsx
@@ -0,0 +1,36 @@
+import classNames from "classnames";
+
+export type Type = "primary" | "secondary";
+
+interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    children?: React.ReactNode;
+    iconOnly?: boolean;
+    color?: Type;
+    transparent?: boolean;
+}
+
+const Button: React.FC<IButtonProps> = ({
+    children,
+    color,
+    iconOnly,
+    transparent,
+    ...props
+}) => {
+    const className = classNames("shadow-inner focus:outline-none ", {
+        "bg-red-500 hover:bg-red-800 text-white shadow-red-400":
+            color === "primary" || !color,
+        "bg-neutral-100 hover:bg-neutral-300 shadow-neutral-50":
+            color === "secondary",
+        "rounded-full p-2": iconOnly,
+        "rounded px-4 py-1.5": !iconOnly,
+        "opacity-75": transparent,
+    });
+
+    return (
+        <button {...props} className={className}>
+            {children}
+        </button>
+    );
+};
+
+export default Button;
