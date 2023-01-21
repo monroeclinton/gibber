1ee6bf2fa28024f08159d2c3ac3d2d62bc3d35f1
Monroe Clinton <contact@monroeclinton.com>
2023-01-21 18:41:13 -0500

    Allow GitHub avatars to be rendered

--- next.config.mjs
+++ next.config.mjs
@@ -13,5 +13,8 @@ const config = {
         locales: ["en"],
         defaultLocale: "en",
     },
+    images: {
+        domains: ["avatars.githubusercontent.com"],
+    },
 };
 export default config;
