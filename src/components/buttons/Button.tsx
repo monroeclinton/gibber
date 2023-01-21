ed5d6e2dd3aa329c778c391c1a2fb1c79114a44a
Monroe Clinton <contact@monroeclinton.com>
2023-01-21 18:41:32 -0500

    primary-outline color for Button component

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,6 +1,6 @@
 import classNames from "classnames";
 
-export type Type = "primary" | "secondary";
+export type Type = "primary" | "primary-outline" | "secondary";
 
 interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     children?: React.ReactNode;
@@ -18,16 +18,18 @@ const Button: React.FC<IButtonProps> = ({
 }) => {
     const className = classNames(
         props.className,
-        "shadow-inner focus:outline-none",
+        "shadow-inner focus:outline-none transition-colors font-semibold",
         {
-            "bg-red-500 hover:bg-red-800 text-white shadow-red-400":
+            "bg-red-500 hover:bg-red-600 text-white shadow-red-400":
                 color === "primary" || !color,
+            "hover:bg-neutral-100 text-red-500 border-2 border-red-500":
+                color === "primary-outline",
             "bg-neutral-100 hover:bg-neutral-200 shadow-neutral-50":
                 color === "secondary",
+            "border-2": !transparent && color === "secondary",
             "rounded-full p-2": iconOnly,
-            "rounded px-4 py-1.5": !iconOnly,
+            "rounded-md px-4 py-0.5": !iconOnly,
             "opacity-75": transparent,
-            "border-2": !transparent,
         }
     );
 
