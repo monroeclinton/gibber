b0ce2f054ee206c0356aec5823302e74e6db920d
Monroe Clinton <contact@monroeclinton.com>
2023-01-20 22:56:31 -0500

    Pass className to Button component

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -16,15 +16,20 @@ const Button: React.FC<IButtonProps> = ({
     transparent,
     ...props
 }) => {
-    const className = classNames("shadow-inner focus:outline-none ", {
-        "bg-red-500 hover:bg-red-800 text-white shadow-red-400":
-            color === "primary" || !color,
-        "bg-neutral-100 hover:bg-neutral-300 shadow-neutral-50":
-            color === "secondary",
-        "rounded-full p-2": iconOnly,
-        "rounded px-4 py-1.5": !iconOnly,
-        "opacity-75": transparent,
-    });
+    const className = classNames(
+        props.className,
+        "shadow-inner focus:outline-none",
+        {
+            "bg-red-500 hover:bg-red-800 text-white shadow-red-400":
+                color === "primary" || !color,
+            "bg-neutral-100 hover:bg-neutral-200 shadow-neutral-50":
+                color === "secondary",
+            "rounded-full p-2": iconOnly,
+            "rounded px-4 py-1.5": !iconOnly,
+            "opacity-75": transparent,
+            "border-2": !transparent,
+        }
+    );
 
     return (
         <button {...props} className={className}>
