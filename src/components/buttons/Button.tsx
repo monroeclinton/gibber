24750d7609d6f113931fdc6907d4e41dd6f964a4
Monroe Clinton <contact@monroeclinton.com>
2023-01-22 09:33:42 -0500

    Adjust Button component styling

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -16,19 +16,28 @@ const Button: React.FC<IButtonProps> = ({
     transparent,
     ...props
 }) => {
+    color = !color ? "primary" : color;
+
     const className = classNames(
         props.className,
-        "shadow-inner focus:outline-none transition-colors font-semibold",
+        "shadow-inner focus:outline-none transition-colors font-semibold box-border",
         {
             "bg-red-500 hover:bg-red-600 text-white shadow-red-400":
-                color === "primary" || !color,
+                color === "primary",
             "hover:bg-neutral-100 text-red-500 border-2 border-red-500":
                 color === "primary-outline",
-            "bg-neutral-100 hover:bg-neutral-200 shadow-neutral-50":
+            "active:ring active:ring-red-100": color.startsWith("primary"),
+            "active:ring-red-100/75":
+                transparent && color.startsWith("primary"),
+        },
+        {
+            "active:ring active:ring-neutral-100 bg-neutral-100 hover:bg-neutral-200 shadow-neutral-50":
                 color === "secondary",
-            "border-2": !transparent && color === "secondary",
+            "active:ring-neutral-100/50": transparent && color === "secondary",
+        },
+        {
             "rounded-full p-2": iconOnly,
-            "rounded-md px-4 py-0.5": !iconOnly,
+            "rounded-md px-4 py-0.5 min-w-[100px] min-h-[32px]": !iconOnly,
             "opacity-75": transparent,
         }
     );
