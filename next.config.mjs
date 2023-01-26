15ab3e781fde7a679f8ebaa75279cc432c893cf6
Monroe Clinton <contact@monroeclinton.com>
2023-01-25 23:03:52 -0500

    Allow gibber bucket to be viewed in Next.js Image

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const config = {
         defaultLocale: "en",
     },
     images: {
-        domains: ["avatars.githubusercontent.com"],
+        domains: ["avatars.githubusercontent.com", "gibber.localhost"],
     },
 };
 export default config;
