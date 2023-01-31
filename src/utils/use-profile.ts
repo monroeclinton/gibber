c8ce95f7b374b9e0580f10efc0c72d736dbd3c31
Monroe Clinton <contact@monroeclinton.com>
2023-01-30 19:29:33 -0500

    Create useProfile hook and helpers

---
+++ src/utils/use-profile.ts
@@ -0,0 +1,53 @@
+import { useEffect } from "react";
+
+import { api } from "./api";
+
+export const clearProfileId = () => {
+    if (typeof window !== typeof undefined) {
+        localStorage.removeItem("profileId");
+        window.dispatchEvent(new Event("storage"));
+    }
+};
+
+export const setProfileId = (id: string) => {
+    if (typeof window !== typeof undefined) {
+        localStorage.setItem("profileId", id);
+        window.dispatchEvent(new Event("storage"));
+    }
+};
+
+export const getProfileId = () => {
+    if (typeof window !== typeof undefined) {
+        return localStorage.getItem("profileId");
+    }
+
+    return null;
+};
+
+export const useProfile = () => {
+    const id = getProfileId();
+
+    const profile = api.profile.getById.useQuery(
+        {
+            id: id ? id : "",
+        },
+        {
+            enabled: !!id,
+        }
+    );
+
+    useEffect(() => {
+        const setProfile = () => {
+            const profileId = getProfileId();
+            if (profileId) {
+                void profile.refetch();
+            }
+        };
+
+        window.addEventListener("storage", setProfile);
+
+        return () => window.addEventListener("storage", setProfile);
+    }, [profile]);
+
+    return profile;
+};
