29f1a3fab8a2e685fd01e75211f87f437f9b45d5
Monroe Clinton <contact@monroeclinton.com>
2023-01-31 21:10:43 -0500

    Remove account router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { accountRouter } from "./routers/account";
 import { postRouter } from "./routers/post";
 import { profileRouter } from "./routers/profile";
 import { createTRPCRouter } from "./trpc";
@@ -9,7 +8,6 @@ import { createTRPCRouter } from "./trpc";
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
-    account: accountRouter,
     post: postRouter,
     profile: profileRouter,
 });
