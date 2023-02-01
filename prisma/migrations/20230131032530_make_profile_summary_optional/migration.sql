a46cc834f2818d96465ddfd4c226c12b9d674193
Monroe Clinton <contact@monroeclinton.com>
2023-01-31 21:09:01 -0500

    Make summary optional

---
+++ prisma/migrations/20230131032530_make_profile_summary_optional/migration.sql
@@ -0,0 +1,22 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Profile" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "avatarId" TEXT,
+    "name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "summary" TEXT,
+    "followingCount" INTEGER NOT NULL DEFAULT 0,
+    "followersCount" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "Profile_avatarId_fkey" FOREIGN KEY ("avatarId") REFERENCES "File" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Profile" ("avatarId", "createdAt", "followersCount", "followingCount", "id", "name", "summary", "updatedAt", "userId", "username") SELECT "avatarId", "createdAt", "followersCount", "followingCount", "id", "name", "summary", "updatedAt", "userId", "username" FROM "Profile";
+DROP TABLE "Profile";
+ALTER TABLE "new_Profile" RENAME TO "Profile";
+CREATE UNIQUE INDEX "Profile_username_key" ON "Profile"("username");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
