74e15b49af21e2c449b9dcb16b6d5935eb3ded13
Monroe Clinton <contact@monroeclinton.com>
2023-02-01 19:10:50 -0500

    ProfileManager atom

--- src/atoms.ts
+++ src/atoms.ts
@@ -1,3 +1,6 @@
 import { atom } from "jotai";
 
 export const navOpenAtom = atom<boolean>(false);
+
+type ProfileManager = "select" | "create" | "edit" | null;
+export const profileManagerAtom = atom<ProfileManager>(null);
