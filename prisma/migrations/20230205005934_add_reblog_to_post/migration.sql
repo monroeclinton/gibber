814790717fc58464ebd67963a8768a7edc5e707e
Monroe Clinton <contact@monroeclinton.com>
2023-02-04 23:49:36 -0500

    Add reblogs to post model

---
+++ prisma/migrations/20230205005934_add_reblog_to_post/migration.sql
@@ -0,0 +1,20 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Post" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "profileId" TEXT NOT NULL,
+    "reblogId" TEXT,
+    "content" TEXT,
+    "repliesCount" INTEGER NOT NULL DEFAULT 0,
+    "reblogsCount" INTEGER NOT NULL DEFAULT 0,
+    "favoritesCount" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Post_profileId_fkey" FOREIGN KEY ("profileId") REFERENCES "Profile" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "Post_reblogId_fkey" FOREIGN KEY ("reblogId") REFERENCES "Post" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_Post" ("content", "createdAt", "favoritesCount", "id", "profileId", "reblogsCount", "repliesCount", "updatedAt") SELECT "content", "createdAt", "favoritesCount", "id", "profileId", "reblogsCount", "repliesCount", "updatedAt" FROM "Post";
+DROP TABLE "Post";
+ALTER TABLE "new_Post" RENAME TO "Post";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
