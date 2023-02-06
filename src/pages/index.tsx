b924c4ac3867947926eb27994c518064af51cdd4
Monroe Clinton <contact@monroeclinton.com>
2023-02-05 23:13:01 -0500

    Create post button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
+import { useAtom } from "jotai";
 import { type NextPage } from "next";
 import Head from "next/head";
 
+import { createPostAtom } from "../atoms";
+import Button from "../components/button";
 import NavButton from "../components/button/NavButton";
 import TopBar from "../components/nav/TopBar";
 import Post from "../components/post";
@@ -9,6 +12,8 @@ import { getProfileId } from "../utils/use-profile";
 
 const Home: NextPage = () => {
     const profileId = getProfileId();
+
+    const [, setCreatePost] = useAtom(createPostAtom);
 
     const posts = api.post.getFeedByProfileId.useQuery(
         {
@@ -29,6 +34,9 @@ const Home: NextPage = () => {
             <TopBar>
                 <NavButton />
                 <p className="ml-5 font-semibold">Latest Posts</p>
+                <Button className="ml-auto" onClick={() => setCreatePost(true)}>
+                    Post
+                </Button>
             </TopBar>
             {posts.data &&
                 posts.data.map((post) => <Post post={post} key={post.id} />)}
