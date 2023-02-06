281e5620a55f3f0da6e1bec6203df1aab1fc1f1a
Monroe Clinton <contact@monroeclinton.com>
2023-02-05 23:10:57 -0500

    Atoms for creating posts

--- src/atoms.ts
+++ src/atoms.ts
@@ -4,3 +4,6 @@ export const navOpenAtom = atom<boolean>(false);
 
 type ProfileManager = "select" | "create" | "edit" | null;
 export const profileManagerAtom = atom<ProfileManager>(null);
+
+export const createPostAtom = atom<boolean>(false);
+export const reblogPostAtom = atom<string | undefined>(undefined);
