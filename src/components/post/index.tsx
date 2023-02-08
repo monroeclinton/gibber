d02372a22f49f9fe2ae6fb7ad8503020f1563628
Monroe Clinton <contact@monroeclinton.com>
2023-02-07 20:02:53 -0500

    Close reblog dropdown on outside click

--- src/components/post/index.tsx
+++ src/components/post/index.tsx
@@ -14,10 +14,11 @@ import { useAtom } from "jotai";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 import { createPostAtom, reblogPostAtom } from "../../atoms";
 import { api } from "../../utils/api";
+import useOutsideClick from "../../utils/use-outside-click";
 import { getProfileId } from "../../utils/use-profile";
 import Attachments from "./Attachments";
 
@@ -73,6 +74,7 @@ const Post: React.FC<{
     const [, setReblogPost] = useAtom(reblogPostAtom);
 
     const [isReblogDropdownOpen, setReblogDropdownOpen] = useState(false);
+    const dropdownRef = useRef<HTMLDivElement>(null);
 
     const reblog = api.reblog.create.useMutation({
         onSuccess: async () => {
@@ -185,6 +187,8 @@ const Post: React.FC<{
         }
     };
 
+    useOutsideClick(() => setReblogDropdownOpen(false), dropdownRef);
+
     if (post.reblog && post.content === null) {
         return <Post reblogger={post.profile} post={post.reblog} />;
     }
@@ -283,7 +287,10 @@ const Post: React.FC<{
                     )}
                     <p>{post.reblogsCount}</p>
                     {isReblogDropdownOpen && (
-                        <div className="absolute left-0 -top-2 mt-2 w-40 rounded-md border border-neutral-200 bg-white shadow">
+                        <div
+                            className="absolute left-0 -top-2 mt-2 w-40 rounded-md border border-neutral-200 bg-white shadow"
+                            ref={dropdownRef}
+                        >
                             <div className="py-1" role="none">
                                 <div
                                     className="block px-4 py-2 transition-all hover:bg-neutral-100"
