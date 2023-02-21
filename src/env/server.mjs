e80643847b77d167f2df9da445e8533f54d45465
Monroe Clinton <contact@monroeclinton.com>
2023-02-20 22:51:28 -0500

    Lint imports

--- src/env/server.mjs
+++ src/env/server.mjs
@@ -3,8 +3,8 @@
  * This file is included in `/next.config.mjs` which ensures the app isn't built with invalid env vars.
  * It has to be a `.mjs`-file to be imported there.
  */
-import { serverSchema } from "./schema.mjs";
 import { env as clientEnv, formatErrors } from "./client.mjs";
+import { serverSchema } from "./schema.mjs";
 
 /**
  * You can't destruct `process.env` as a regular object, so we do
