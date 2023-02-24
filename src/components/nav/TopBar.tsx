41c99ab0a887ae488c05c5ddb7768dcff0a4cfa5
Monroe Clinton <contact@monroeclinton.com>
2023-02-23 21:49:50 -0500

    Allow specify mobileOnly on TopBar

--- src/components/nav/TopBar.tsx
+++ src/components/nav/TopBar.tsx
@@ -1,10 +1,16 @@
+import classNames from "classnames";
+
 interface ITopBarProps {
+    mobileOnly?: boolean;
     children: React.ReactNode;
 }
 
-const TopBar: React.FC<ITopBarProps> = ({ children }) => {
+const TopBar: React.FC<ITopBarProps> = ({ mobileOnly, children }) => {
+    const className = classNames("h-[50px]", {
+        "lg:hidden": mobileOnly,
+    });
     return (
-        <div className="h-[50px]">
+        <div className={className}>
             <div className="fixed flex h-[50px] w-full">
                 <div className="flex grow items-center bg-white px-6 py-1 shadow">
                     {children}
