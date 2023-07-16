15ac2a4dce9a37a0a2375356deb604568055b088
Monroe Clinton <contact@monroeclinton.com>
2023-07-15 21:56:21 -0400

    Set custom Tailwind screen based on container sizes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
     content: ["./src/**/*.{js,ts,jsx,tsx}", "./assets/**/*.svg"],
     theme: {
-        extend: {},
+        extend: {
+            screens: {
+                // Minimum for full desktop
+                // 615px - post container
+                // 320px (container) + 40px (margin) - two sidebars
+                fd: "1335px",
+            },
+        },
     },
     plugins: [require("@tailwindcss/aspect-ratio")],
 };
