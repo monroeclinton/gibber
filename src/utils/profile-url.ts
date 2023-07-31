9c7019bc8fa84c7beee9f90a676fc486b08dd363
Monroe Clinton <contact@monroeclinton.com>
2023-07-30 21:44:19 -0400

    Use NEXT_PUBLIC env

--- src/utils/profile-url.ts
+++ src/utils/profile-url.ts
@@ -1,7 +1,7 @@
 import { env } from "../env.mjs";
 
 export const profileUrl = (username: string, domain: string) => {
-    return env.WEB_DOMAIN === domain
+    return env.NEXT_PUBLIC_WEB_DOMAIN === domain
         ? `/${username}`
         : `/${username}@${domain}`;
 };
