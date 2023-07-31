dd2c9a87da1884f6203d7186e0bfcf29b5182e66
Monroe Clinton <contact@monroeclinton.com>
2023-07-30 21:50:24 -0400

    Migration to add url to profile

---
+++ prisma/migrations/20230731012759_add_url_to_profile/migration.sql
@@ -0,0 +1,33 @@
+/*
+  Warnings:
+
+  - Added the required column `url` to the `Profile` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Profile" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT,
+    "headerId" TEXT,
+    "avatarId" TEXT,
+    "name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "domain" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "summary" TEXT,
+    "followingCount" INTEGER NOT NULL DEFAULT 0,
+    "followersCount" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "Profile_headerId_fkey" FOREIGN KEY ("headerId") REFERENCES "File" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "Profile_avatarId_fkey" FOREIGN KEY ("avatarId") REFERENCES "File" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Profile" ("avatarId", "createdAt", "domain", "followersCount", "followingCount", "headerId", "id", "name", "summary", "updatedAt", "userId", "username") SELECT "avatarId", "createdAt", "domain", "followersCount", "followingCount", "headerId", "id", "name", "summary", "updatedAt", "userId", "username" FROM "Profile";
+DROP TABLE "Profile";
+ALTER TABLE "new_Profile" RENAME TO "Profile";
+CREATE UNIQUE INDEX "Profile_url_key" ON "Profile"("url");
+CREATE UNIQUE INDEX "Profile_username_domain_key" ON "Profile"("username", "domain");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
